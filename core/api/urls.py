4747338e32f33dd5ea535c49f18a4269ba8ddb37
Aseem Gupta <aseemguptajaipur@gmail.com>
2024-05-02 15:28:58 +0530

    people serailizer aur model aur uske corresponding views me function aur url likha hai

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,7 +1,7 @@
-from home.views import index
-
+from home.views import index,people
 from django.urls import path
 
 urlpatterns = [
-   path('index/',index,name = 'index')
+   path('index/',index,name = 'index'),
+   path('person/',people)
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from home.models import Person
+from home.serializers import PeopleSerializer
 
 @api_view(['GET','POST'])
 def index(request):
@@ -9,9 +11,28 @@ def index(request):
                'teacher':'abhijeet bhaiya'}
     if request.method == 'GET':
         print('get method called')
+        data = request.GET.get('search')
+        print(f'\n\n\n\n\n{data}\n\n\n\n\n')
+
     elif request.method == 'POST':
+        data = request.data
+        print(f'\n\n\n\n\n{data} and to be specific {data['name']}\n\n\n\n\n')
         print('post method called')
     else:
         print('ran into some problem')
     return Response(courses)
 # Create your views here.
+
+@api_view(['GET','POST'])
+def people(request):
+    if request.method == 'GET':
+        objs = Person.objects.all()
+        serializer = PeopleSerializer(objs,many = True)
+        return Response(serializer.data)
+    else: # post method
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
